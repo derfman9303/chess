ba9070ffcf7295bb838721cc7a4ca7b1712730ce
Frederic Hodges <frederichodges@gmail.com>
2022-06-30 20:19:20 -0500

    Add knight

--- chess.js
+++ chess.js
@@ -100,6 +100,10 @@ class Chess {
             new Rook(this.grid, 7, 7, "white", this.turn, this.pieces),
             new Rook(this.grid, 0, 0, "black", this.turn, this.pieces),
             new Rook(this.grid, 0, 7, "black", this.turn, this.pieces),
+            new Knight(this.grid, 7, 1, "white", this.turn, this.pieces),
+            new Knight(this.grid, 7, 6, "white", this.turn, this.pieces),
+            new Knight(this.grid, 0, 1, "black", this.turn, this.pieces),
+            new Knight(this.grid, 0, 6, "black", this.turn, this.pieces),
             new Bishop(this.grid, 7, 2, "white", this.turn, this.pieces),
             new Bishop(this.grid, 7, 5, "white", this.turn, this.pieces),
             new Bishop(this.grid, 0, 2, "black", this.turn, this.pieces),
@@ -548,6 +552,131 @@ class Rook extends Chess {
                 break;
             }
             s++;
+        }
+    }
+}
+
+class Knight extends Chess {
+    constructor(grid, row, square, color, turn, pieces) {
+        super(grid, turn, pieces);
+        this.row    = row;
+        this.square = square;
+        this.color  = color;
+        this.icon   = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - 
+                        https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="` + color + `" d="M19 272.5l40.62 18C63.78 292.3 68.25 293.3 72.72 
+                        293.3c4 0 8.001-.7543 11.78-2.289l12.75-5.125c9.125-3.625 16-11.12 18.75-20.5L125.2 234.8C127 227.9 131.5 222.2 137.9 219.1L160 208v50.38C160 276.5 
+                        149.6 293.1 133.4 301.2L76.25 329.9C49.12 343.5 32 371.1 32 401.5V416h319.9l-.0417-192c0-105.1-85.83-192-191.8-192H12C5.375 32 0 37.38 0 44c0 2.625 
+                        .625 5.25 1.75 7.625L16 80L7 89C2.5 93.5 0 99.62 0 106V243.2C0 255.9 7.5 267.4 19 272.5zM52 128C63 128 72 137 72 148S63 168 52 168S32 159 32 148S41 
+                        128 52 128zM336 448H47.1C21.49 448 0 469.5 0 495.1C0 504.8 7.163 512 16 512h352c8.837 0 16-7.163 16-16C384 469.5 362.5 448 336 448z"/></svg>`;
+
+        this.initializePiece(this.row, this.square);
+    }
+
+    move(row, square, pieces) {
+        this.vacateSquare(this.row, this.square);
+
+        // If there is already a piece on the square (capture)
+        for (let p = 0; p < pieces.length; p++) {
+            if (pieces[p].row === row && pieces[p].square === square) {
+                pieces[p].row = -1;
+                pieces[p].square = -1;
+            }
+        }
+
+        // Update piece position
+        this.row = row;
+        this.square = square;
+
+        this.initializePiece(this.row, this.square);
+    }
+
+    validMoves(turn) {
+        // up 1
+        let r = this.row - 2;
+        let s = this.square - 1;
+        if (r >= 0 && s >= 0) {
+            if (this.grid[r][s].getAttribute("data-value") == "") {
+                this.grid[r][s].classList.add("highlighted");
+            } else if (this.grid[r][s].getAttribute("data-value") !== turn) {
+                this.captureHighlighting(r, s, this.row, this.square, turn);
+            }
+        }
+
+        // up 2
+        r = this.row - 2;
+        s = this.square + 1;
+        if (r >= 0 && s < 8) {
+            if (this.grid[r][s].getAttribute("data-value") == "") {
+                this.grid[r][s].classList.add("highlighted");
+            } else if (this.grid[r][s].getAttribute("data-value") !== turn) {
+                this.captureHighlighting(r, s, this.row, this.square, turn);
+            }
+        }
+
+        // down 1
+        r = this.row + 2;
+        s = this.square - 1;
+        if (r < 8 && s >= 0) {
+            if (this.grid[r][s].getAttribute("data-value") == "") {
+                this.grid[r][s].classList.add("highlighted");
+            } else if (this.grid[r][s].getAttribute("data-value") !== turn) {
+                this.captureHighlighting(r, s, this.row, this.square, turn);
+            }
+        }
+
+        // down 2
+        r = this.row + 2;
+        s = this.square + 1;
+        if (r < 8 && s < 8) {
+            if (this.grid[r][s].getAttribute("data-value") == "") {
+                this.grid[r][s].classList.add("highlighted");
+            } else if (this.grid[r][s].getAttribute("data-value") !== turn) {
+                this.captureHighlighting(r, s, this.row, this.square, turn);
+            }
+        }
+
+        // left 1
+        r = this.row + 1;
+        s = this.square - 2;
+        if (r < 8 && s >= 0) {
+            if (this.grid[r][s].getAttribute("data-value") == "") {
+                this.grid[r][s].classList.add("highlighted");
+            } else if (this.grid[r][s].getAttribute("data-value") !== turn) {
+                this.captureHighlighting(r, s, this.row, this.square, turn);
+            }
+        }
+
+        // left 2
+        r = this.row - 1;
+        s = this.square - 2;
+        if (r >= 0 && s >= 0) {
+            if (this.grid[r][s].getAttribute("data-value") == "") {
+                this.grid[r][s].classList.add("highlighted");
+            } else if (this.grid[r][s].getAttribute("data-value") !== turn) {
+                this.captureHighlighting(r, s, this.row, this.square, turn);
+            }
+        }
+
+        // right 1
+        r = this.row - 1;
+        s = this.square + 2;
+        if (r >= 0 && s < 8) {
+            if (this.grid[r][s].getAttribute("data-value") == "") {
+                this.grid[r][s].classList.add("highlighted");
+            } else if (this.grid[r][s].getAttribute("data-value") !== turn) {
+                this.captureHighlighting(r, s, this.row, this.square, turn);
+            }
+        }
+
+        // right 2
+        r = this.row + 1;
+        s = this.square + 2;
+        if (r < 8 && s < 8) {
+            if (this.grid[r][s].getAttribute("data-value") == "") {
+                this.grid[r][s].classList.add("highlighted");
+            } else if (this.grid[r][s].getAttribute("data-value") !== turn) {
+                this.captureHighlighting(r, s, this.row, this.square, turn);
+            }
         }
     }
 }
